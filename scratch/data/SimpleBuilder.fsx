e72818742245e9e676d03e3e288d33936627e9a4
Tom Davies <todavies5@gmail.com>
2018-09-05 11:49:00 +0100

    Fix type issues in the example builder .fsx

--- scratch/data/SimpleBuilder.fsx
+++ scratch/data/SimpleBuilder.fsx
@@ -9,15 +9,15 @@ type OptionalBuilder =
         | None -> None
     
     // TODO Make this actually get called when `let! ... and! ...` syntax is used (and automagically if RHSs allow it?)
-    member __.Apply(x : 'a option, f : ('a -> 'b) option) : 'b option =
-        match f, x with
-        | Some f, Some x -> f x
+    member __.Apply(xOpt : 'a option, fOpt : ('a -> 'b) option) : 'b option =
+        match fOpt, xOpt with
+        | Some f, Some x -> Some (f x)
         | _ -> None
 
     // TODO Not needed, but for maximum efficiency, we want to use this if it is defined
-    member __.Map(x : 'a option, f : 'a -> 'b) : 'b option =
-        match x with
-        | Some x -> f x
+    member __.Map(xOpt : 'a option, f : 'a -> 'b) : 'b option =
+        match xOpt with
+        | Some x -> Some (f x)
         | None -> None
 
     member __.Return(x) =
